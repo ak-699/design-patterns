0487c7b812fcaffcd42a83593f45cfda95cb3ea8
ak-699 <kumar699.abhishek@gmail.com>
2025-09-06 13:46:00 +0530

    feat: facade - room service subsystem

---
+++ structural/decorator/facade/service/RoomService.java
@@ -0,0 +1,11 @@
+package structural.decorator.facade.service;
+
+public class RoomService {
+    public void assignRoom(String guestName, String roomNo) {
+        System.out.println("Room " + roomNo + " booked for " + guestName);
+    }
+
+    public void cleanRoom(String roomNo) {
+        System.out.println("Room " + roomNo + " cleaned.");
+    }
+}
