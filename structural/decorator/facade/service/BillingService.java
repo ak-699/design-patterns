6cc3c9c45da9a4a3e3212ccae264bcb855011d22
ak-699 <kumar699.abhishek@gmail.com>
2025-09-06 13:48:05 +0530

    feat: facade - billing service subsystem

---
+++ structural/decorator/facade/service/BillingService.java
@@ -0,0 +1,11 @@
+package structural.decorator.facade.service;
+
+public class BillingService {
+    public void payDeposit(String guest, double amount) {
+        System.out.println(guest + " paid deposity Rs." + amount);
+    }
+
+    public void generateInvoice(String guest, double total) {
+        System.out.println("Invoice generated for " + guest + " of Total: " + total);
+    }
+}
