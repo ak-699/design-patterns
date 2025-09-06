2956ca74046cea3818654eed6c4e986a68d28d22
ak-699 <kumar699.abhishek@gmail.com>
2025-09-06 12:45:24 +0530

    feat: coffee decorator

---
+++ structural/decorator/coffee_shop/CoffeeDecorator.java
@@ -0,0 +1,19 @@
+package structural.decorator.coffee_shop;
+
+public abstract class CoffeeDecorator implements Coffee {
+    private Coffee decoratedCoffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.decoratedCoffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedCoffee.getCost();
+    }
+}
