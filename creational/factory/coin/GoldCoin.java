2af05ab19c7a21440ed49e576a2791f264d74807
ak-699 <kumar699.abhishek@gmail.com>
2025-09-07 17:53:30 +0530

    feat: factory - gold coin

---
+++ creational/factory/coin/GoldCoin.java
@@ -0,0 +1,10 @@
+package creational.factory.coin;
+
+public class GoldCoin implements Coin {
+
+    @Override
+    public String getDescription() {
+        return "This is a gold coin";
+    }
+
+}
