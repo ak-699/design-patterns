9a824d57699418f5abd5dba1082149e6eb108252
ak-699 <kumar699.abhishek@gmail.com>
2025-09-07 17:53:43 +0530

    feat: factory - copper coin

---
+++ creational/factory/coin/CopperCoin.java
@@ -0,0 +1,10 @@
+package creational.factory.coin;
+
+public class CopperCoin implements Coin {
+
+    @Override
+    public String getDescription() {
+        return "This is a copper coin";
+    }
+
+}
