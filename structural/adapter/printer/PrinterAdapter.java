1de76dd72423a04cffafe3c51a4c2c94d2fe66ed
ak-699 <kumar699.abhishek@gmail.com>
2025-09-08 19:45:23 +0530

    feat: adapter - printer adapter

---
+++ structural/adapter/printer/PrinterAdapter.java
@@ -0,0 +1,15 @@
+package structural.adapter.printer;
+
+public class PrinterAdapter implements Printer {
+    private OldPrinter oldPrinter;
+
+    public PrinterAdapter(OldPrinter oldPrinter) {
+        this.oldPrinter = oldPrinter;
+    }
+
+    @Override
+    public void print(String content) {
+        oldPrinter.print(content);
+    }
+
+}
