525e495015d5f11c017347ca4564e8f6701f8ade
ak-699 <kumar699.abhishek@gmail.com>
2025-09-08 20:52:02 +0530

    feat: bridge - abstract message

---
+++ structural/bridge/message/Message.java
@@ -0,0 +1,11 @@
+package structural.bridge.message;
+
+public abstract class Message {
+    protected MessageSender messageSender;
+
+    public Message(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public abstract void send(String message);
+}
