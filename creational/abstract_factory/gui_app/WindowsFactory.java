73abc0f2f7fbac501ef7d590078f077f03db4336
ak-699 <kumar699.abhishek@gmail.com>
2025-09-11 22:20:29 +0530

    feat: windows factory impl

---
+++ creational/abstract_factory/gui_app/WindowsFactory.java
@@ -0,0 +1,15 @@
+package creational.abstract_factory.gui_app;
+
+public class WindowsFactory implements GUIFactory {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+
+}
