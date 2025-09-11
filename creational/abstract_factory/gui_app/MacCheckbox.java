179ee8c2d573d8ba3983bdaaaa69cf7e1d7b2961
ak-699 <kumar699.abhishek@gmail.com>
2025-09-11 22:19:20 +0530

    feat: mac checkbox

---
+++ creational/abstract_factory/gui_app/MacCheckbox.java
@@ -0,0 +1,10 @@
+package creational.abstract_factory.gui_app;
+
+public class MacCheckbox implements Checkbox {
+
+    @Override
+    public void onCheck() {
+        System.out.println("MAC CHECKBOX.");
+    }
+
+}
