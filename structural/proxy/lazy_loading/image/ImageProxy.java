ab3afd23da6e9dce48c365aa83120c71288a6539
ak-699 <kumar699.abhishek@gmail.com>
2025-09-11 21:22:40 +0530

    feat: proxy - image proxy

---
+++ structural/proxy/lazy_loading/image/ImageProxy.java
@@ -0,0 +1,21 @@
+package structural.proxy.lazy_loading.image;
+
+public class ImageProxy implements Image {
+
+    private RealImage realImage;
+    private String filename;
+
+    public ImageProxy(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+
+        }
+        realImage.display();
+    }
+
+}
