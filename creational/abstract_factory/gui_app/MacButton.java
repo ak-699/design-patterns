af7902115fb361fd6fda781c6c20daa029eaa200
ak-699 <kumar699.abhishek@gmail.com>
2025-09-11 22:19:10 +0530

    feat: mac button

---
+++ creational/abstract_factory/gui_app/MacButton.java
@@ -0,0 +1,10 @@
+package creational.abstract_factory.gui_app;
+
+public class MacButton implements Button {
+
+    @Override
+    public void onPress() {
+        System.out.println("MAC BUTTON.");
+    }
+
+}
