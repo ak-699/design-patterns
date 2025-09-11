4964a409c7f14b21913b8fc533938eafb4e17350
ak-699 <kumar699.abhishek@gmail.com>
2025-09-11 22:19:29 +0530

    feat: windows button

---
+++ creational/abstract_factory/gui_app/WindowsButton.java
@@ -0,0 +1,10 @@
+package creational.abstract_factory.gui_app;
+
+public class WindowsButton implements Button {
+
+    @Override
+    public void onPress() {
+        System.out.println("WINDOWS BUTTON.");
+    }
+
+}
