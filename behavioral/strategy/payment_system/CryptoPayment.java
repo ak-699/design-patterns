a81c6493a004980f602ca6354dee6f2ef7d17f85
ak-699 <kumar699.abhishek@gmail.com>
2025-09-29 10:43:42 +0530

    feat: crypto payment

---
+++ behavioral/strategy/payment_system/CryptoPayment.java
@@ -0,0 +1,15 @@
+package behavioral.strategy.payment_system;
+
+public class CryptoPayment implements PaymentStrategy {
+    private String walletAddress;
+
+    public CryptoPayment(String walletAddress) {
+        this.walletAddress = walletAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("paying via crypto " + walletAddress);
+    }
+
+}
