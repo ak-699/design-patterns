67eb77d1ba8fa8ed77a40b18faaf109d83ce1616
ak-699 <kumar699.abhishek@gmail.com>
2025-09-29 10:43:52 +0530

    feat: paypal payment

---
+++ behavioral/strategy/payment_system/PayPalPayment.java
@@ -0,0 +1,15 @@
+package behavioral.strategy.payment_system;
+
+public class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("paying via paypal " + email);
+    }
+
+}
